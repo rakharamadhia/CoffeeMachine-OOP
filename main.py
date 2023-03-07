a9fe2b17d1c3b6801a7b4e307e0c60e548b5708b
rakharamadhia <rakharamadhia@gmail.com>
2023-03-07 10:27:18 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+money_machine.report()
+coffee_maker.report()
